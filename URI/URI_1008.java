aab66c7b91f5ac13635639d14d1937fbaaebf921
Alan Santos <alanxdsi@gmail.com>
2021-04-10 10:57:30 -0300

    Exercise 1008

---
+++ URI/URI_1008.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+/*
+ *  Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. 
+ *  Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
+ *
+ *   Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
+ *   Don’t forget the space before and after the equal signal and after the U$.
+ *   Input
+ *   The input file contains 2 integer numbers and 1 value of floating point, representing the number, worked hours amount and the amount the employee receives per worked hour.
+ *
+ *  Output
+ *   Print the number and the employee's salary, according to the given example, with a blank space before and after the equal signal.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class URI_1008 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int A = sc.nextInt();
+		 int B = sc.nextInt();
+		 double C = sc.nextDouble();
+		 double salary = (B * C);
+	 	 System.out.println("NUMBER = " + A);
+		 System.out.printf("SALARY = U$ %.2f\n", salary);
+		
+		sc.close();
+	}
+}
