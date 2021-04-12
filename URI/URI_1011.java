daff4d939cba1e114130cfc7fbf154fa0cbbac0c
Alan Santos <alanxdsi@gmail.com>
2021-04-12 12:40:49 -0300

    Create URI_1011.java

---
+++ URI/URI_1011.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/*
+Make a program that calculates and shows the volume of a sphere being provided the value of its radius (R).
+The formula to calculate the volume is: (4/3) * pi * R3. Consider (assign) for pi the value 3.14159.
+
+Tip: Use (4/3.0) or (4.0/3) in your formula, because some languages (including C++) assume that the division's result between two integers is another integer. :)
+
+Input
+The input contains a value of floating point (double precision).
+
+Output
+The output must be a message "VOLUME" like the following example with a space before and after the equal signal. The value must be presented with 3 digits after the decimal point.
+ */
+public class URI_1011 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double PI = 3.14159;
+		double radius = sc.nextInt();
+		double volume = (4.0/3) * PI * (Math.pow(radius, 3));
+		
+		System.out.printf("VOLUME = %.3f\n", volume);
+
+		sc.close();
+
+	}
+
+}
