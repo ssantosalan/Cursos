97d43910763d7477e3a950e4f37d886a30d797cb
Alan Santos <alanxdsi@gmail.com>
2021-04-14 10:25:04 -0300

    Create URI_1014.java

---
+++ URI/URI_1014.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
+
+Input
+The input file contains two values: one integer value X representing the total distance (in Km) and the second one is a floating point number Y  representing the spent fuel total, with a digit after the decimal point.
+
+Output
+Present a value that represents the average consumption of a car with 3 digits after the decimal point, followed by the message "km/l".
+  
+ */
+public class URI_1014 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		double Y = sc.nextDouble();
+
+		double consumption = (X / Y);
+
+		System.out.printf("%.3f km/l\n", consumption);
+
+		sc.close();
+
+	}
+
+}
