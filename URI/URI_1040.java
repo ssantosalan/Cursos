3675c528f6024c2a4a898a45732f5b8b0230f1de
Alan Santos <ss.alan@outlook.com>
2021-04-25 13:56:00 -0300

    Update URI_1040.java

--- URI/URI_1040.java
+++ URI/URI_1040.java
@@ -1,40 +1,46 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+/*
+
+Read four numbers (N1, N2, N3, N4), which one with 1 digit after the decimal point, corresponding to 4 scores obtained by a student. Calculate the average with weights 2, 3, 4 e 1 respectively, for these 4 scores and print the message "Media: " (Average), followed by the calculated result. If the average was 7.0 or more, print the message "Aluno aprovado." (Approved Student). If the average was less than 5.0, print the message: "Aluno reprovado." (Reproved Student). If the average was between 5.0 and 6.9, including these, the program must print the message "Aluno em exame." (In exam student).
+
+In case of exam, read one more score. Print the message "Nota do exame: " (Exam score) followed by the typed score. Recalculate the average (sum the exam score with the previous calculated average and divide by 2) and print the message “Aluno aprovado.” (Approved student) in case of average 5.0 or more) or "Aluno reprovado." (Reproved student) in case of average 4.9 or less. For these 2 cases (approved or reproved after the exam) print the message "Media final: " (Final average) followed by the final average for this student in the last line.
+Input
+
+The input contains four floating point numbers that represent the students' grades.
+Output
+
+Print all the answers with one digit after the decimal point.
+
+ */
 public class URI_1040 {
 
 	public static void main(String[] args) {
-		DecimalFormat df = new DecimalFormat("0.0");
 		Scanner sc = new Scanner(System.in);
-		double n1 = sc.nextDouble();
-		double n2 = sc.nextDouble();
-		double n3 = sc.nextDouble();
-		double n4 = sc.nextDouble();
-		double media = ((n1 * 2.0) + (n2 * 3.0) + (n3 * 4.0) + (n4 * 1.0)) / 10.0;
-		System.out.printf("Media: " + df.format(media) + "\n");
-
-		if (media >= 7) {
+		DecimalFormat df = new DecimalFormat("0.0");
+		double N1 = sc.nextDouble();
+		double N2 = sc.nextDouble();
+		double N3 = sc.nextDouble();
+		double N4 = sc.nextDouble();
+		double MEDIA = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10;
+		System.out.println("Media: " + df.format(MEDIA));
+		if (MEDIA >= 7) {
 			System.out.println("Aluno aprovado.");
-		} else if (media < 5) {
-			System.out.println("Aluno reprovado.");
-			System.out.println();
-		} else if (media >= 5.0 || media <= 6.9) {
+		} else if (MEDIA < 7 && MEDIA >= 5) {
 			System.out.println("Aluno em exame.");
-			n1 = sc.nextDouble();
-			System.out.printf("Nota do exame: " + df.format(n1) + "\n");
-			media = (media + n1) / 2;
-
-			if (media >= 5.0) {
+			double NExame = sc.nextDouble();
+			System.out.println("Nota do exame: " + df.format(NExame));
+			MEDIA = (MEDIA + NExame) / 2;
+			if (MEDIA >= 5) {
 				System.out.println("Aluno aprovado.");
 			} else {
 				System.out.println("Aluno reprovado.");
 			}
-			System.out.printf("Media final: " + df.format(media) + "\n");
-
+			System.out.println("Media final: " + df.format(MEDIA));
+		} else {
+			System.out.println("Aluno reprovado.");
 		}
-
-		sc.close();
-
 	}
 
 }
