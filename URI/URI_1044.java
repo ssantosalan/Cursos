9829a6eea7c11d9c7e6ab0245f0f1b3908112dca
Alan Santos <ss.alan@outlook.com>
2021-04-29 10:13:13 -0300

    Update URI_1044.java

--- URI/URI_1044.java
+++ URI/URI_1044.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /*
 
 
-Read two nteger values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+Read two Integer values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
 Input
 
 The input has two integer numbers.
