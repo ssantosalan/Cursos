7384051bef0b3cc5400afd9e933c98b8ad459b04
Alan Santos <ss.alan@outlook.com>
2021-05-07 17:06:39 -0300

    exercicios-aula-14

---
+++ java-cursoemvideo/aula-14-vetores/Vetor01.java
@@ -0,0 +1,13 @@
+
+public class Vetor01 {
+
+	public static void main(String[] args) {
+		int n [] = {3, 2, 8, 7, 5, 4};
+		
+		
+		for(int c = 0; c< n.length; c++) {
+			System.out.println("Na posição " + c + " temos o valor: " + n[c] );
+		}
+	}
+
+}
---
+++ java-cursoemvideo/aula-14-vetores/Vetor03.java
@@ -0,0 +1,14 @@
+
+public class Vetor03 {
+
+	public static void main(String[] args) {
+		
+		int vetor[] = {0,1,2,6,4};
+		
+		for (int valor : vetor) {
+			System.out.println(valor);
+		}
+
+	}
+
+}
