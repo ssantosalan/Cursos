e496bc12238545a66651e2aa07884f09d4f4154c
Alan Santos <ss.alan@outlook.com>
2021-05-20 14:44:49 -0300

    Create Aula_15.java

---
+++ java-cursoemvideo/aula-15-metodos/Aula_15.java
@@ -0,0 +1,14 @@
+
+public class Aula_15 {
+
+	static int soma(int a, int b) {
+		int s = a + b;
+		return s;
+	}
+	
+	public static void main(String[] args) {
+		int sm = soma(5, 2);
+		System.out.println("Soma: " + sm);
+	}
+
+}
