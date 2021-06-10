3e1f3f4ec9001b8c2b6086b403ea6fe24ca3ddd4
Alan Santos <ss.alan@outlook.com>
2021-06-10 13:25:21 -0300

    Create URI_1061.java

---
+++ URI/URI_1061.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class URI_1061 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int diaComeço = entrada.nextInt();
+		
+		int horaComeço = entrada.nextInt();
+		
+		int minutoComeço = entrada.nextInt();
+	
+		int segundoComeço = entrada.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		entrada.close();
+
+	}
+
+}
