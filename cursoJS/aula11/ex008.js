9cb2ea67468ebaeb454851907752c0dbe151305f
Alan Santos <alanxdsi@gmail.com>
2021-09-06 19:48:19 -0300

    ex008

---
+++ cursoJS/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+var maxvel = 60;
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if(vel > maxvel) {
+    console.log(`Você foi multado, pois sua velocidade foi de ${vel}, enquanto a velocidade máxima era ${maxvel}!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
