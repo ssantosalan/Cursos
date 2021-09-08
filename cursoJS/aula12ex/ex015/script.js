bfbabd66936f0c4023e5c59bba91933325c454b9
Alan Santos <alanxdsi@gmail.com>
2021-09-08 14:05:35 -0300

    Update script.js

--- cursoJS/aula12ex/ex015/script.js
+++ cursoJS/aula12ex/ex015/script.js
@@ -9,7 +9,49 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 2) {
+                //bebê
+                img.setAttribute('src', 'baby-m.png')
+            } else if (idade < 13) {
+                //criança
+                img.setAttribute('src', 'child-m.png')
+            } else if (idade < 20) {
+                // adolescente
+                img.setAttribute('src', 'teenager-m.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adult-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'older-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 2) {
+                //bebê
+                img.setAttribute('src', 'baby-f.png')
+            } else if (idade < 13) {
+                //criança
+                img.setAttribute('src', 'child-f.png')
+            } else if (idade < 20) {
+                // adolescente
+                img.setAttribute('src', 'teenager-f.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adult-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'older-f.png')
+            }
+        } 
+        res.style.textAlign = 'center'
+        res.innerHTML = `${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 
 }
