4dd02be8b9628ae34a2533c95dd233b92fb99670
Alan Santos <alanxdsi@gmail.com>
2021-09-08 13:46:53 -0300

    imgs

---
+++ cursoJS/aula12ex/ex015/script.js
@@ -0,0 +1,15 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO]')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade ${idade}`
+    }
+
+}
