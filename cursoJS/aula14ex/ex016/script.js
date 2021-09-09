0ad8b2853b2eb351ca8f34f16f09a41290bbaebe
Alan Santos <alanxdsi@gmail.com>
2021-09-09 18:20:22 -0300

    Update script.js

--- cursoJS/aula14ex/ex016/script.js
+++ cursoJS/aula14ex/ex016/script.js
@@ -1,5 +1,5 @@
 function contar(){
-
+   
     var txtinicio = document.querySelector('input#txtinicio')
     var inicio = Number(txtinicio.value)
     var txtfim = document.querySelector('input#txtfim')
@@ -8,21 +8,21 @@ function contar(){
     var passo = Number(txtpasso.value)
     var res = document.querySelector('div#res')
     
-
-    
-
     res.innerHTML = `<p>Contando:</p>`
+
+    if (inicio === 0) {
+        res.innerHTML= 'Impossível contar!'
+    }
+
+    if (passo === 0 && inicio != 0) {
+        window.alert('Valor inválido! Será considerado passo 1')
+        passo = 1
+    }
     
     for (var i = inicio; i <= fim; i = i + passo) {
         res.innerHTML = i
     }
-        
     
     
-    
-
-
-
-
 
 }
