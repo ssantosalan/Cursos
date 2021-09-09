83d19a660910dccc1bd16c1b217ac1981a1edb46
Alan Santos <alanxdsi@gmail.com>
2021-09-09 17:27:22 -0300

    ex

---
+++ cursoJS/aula14ex/ex016/script.js
@@ -0,0 +1,17 @@
+function contar(){
+
+    var txtinicio = document.querySelector('input#txtinicio')
+    var inicio = Number(txtinicio.value)
+    var txtfim = document.querySelector('input#txtfim')
+    var fim = Number(txtfim.value)
+    var res = document.querySelector('div#res')
+    
+
+    res.innerHTML = `${inicio}`
+    res.innerHTML = `${fim}`
+
+
+
+
+
+}
