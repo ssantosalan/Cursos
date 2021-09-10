92bf3b6fe2d1b0420af5d3d3404c76f18a973010
Alan Santos <alanxdsi@gmail.com>
2021-09-10 13:20:46 -0300

    ex tabuada

--- cursoJS/aula14ex/ex017/script.js
+++ cursoJS/aula14ex/ex017/script.js
@@ -1,6 +1,12 @@
 function gerar() {
 
+    var txtnumero = document.querySelector('input#txtnumero')
+    var numero = Number(txtnumero.value)
+    var res = document.querySelector('div#res')
     
+    for (var i = 1; i <= 10; i++) {
+        res.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>   `
 
+    }
 
 }
