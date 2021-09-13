02dbeea6058c94b2f1110b6c0cd7d6700cbdadc0
Alan Santos <alanxdsi@gmail.com>
2021-09-13 11:30:05 -0300

    array

---
+++ cursoJS/aula15/ambiente.js
@@ -0,0 +1,4 @@
+let num = [5, 2, 9, 3]
+num[4] = 6
+console.log(`Nosso vetor tem ${num.length} elementos`)
+console.log(num)
---
+++ cursoJS/aula15/vetornatela.js
@@ -0,0 +1,8 @@
+let valores = [8, 1, 2, 6, 4, 9]
+valores.sort()
+console.log(valores)
+
+
+for(let i = 0; i < valores.length; i++) {
+    console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
