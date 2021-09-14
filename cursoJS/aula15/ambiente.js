3c8b4573a0be2caa8184d4c291d5e2057fd5b88a
Alan Santos <alanxdsi@gmail.com>
2021-09-14 11:48:44 -0300

    ex 16

--- cursoJS/aula15/ambiente.js
+++ cursoJS/aula15/ambiente.js
@@ -1,4 +1,12 @@
 let num = [5, 2, 9, 3]
 num[4] = 6
+num.push(1)
+num.sort()
 console.log(`Nosso vetor tem ${num.length} elementos`)
 console.log(num)
+let pos = num.indexOf(2)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 2 está na posição ${pos}`)
+}
