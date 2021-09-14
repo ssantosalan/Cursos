9553cdc0b91b1779b019d2ec481e9048c49fec72
Alan Santos <alanxdsi@gmail.com>
2021-09-14 18:38:44 -0300

    att ex

--- cursoJS/aula16ex/exfuncao/script.js
+++ cursoJS/aula16ex/exfuncao/script.js
@@ -1,6 +1,33 @@
-function adicionar() {
-    let txtnumero = document.querySelector('input#txtnumero')
-    let res = document.querySelector('div#res')
-    let numero = Number(txtnumero.value)
-    res.innerHTML = numero
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return !n
+    }
 }
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return !n
+    }
+}
+
+function adicionar() {
+
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+
+}
+
+
+
