8bba110aaa6e01bd1393f6d8c0ec421471381542
Alan Santos <alanxdsi@gmail.com>
2021-09-22 18:18:32 -0300

    js

---
+++ every-day-a-page/page-gero/js/scripts.js
@@ -0,0 +1,15 @@
+const hospedes = [
+	{ id: '1', nome: 'Antônio', nascimento: '1950-05-10', ativo: true},
+	{ id: '2', nome: 'Mario', nascimento: '1940-03-10', ativo: false},
+	{ id: '3', nome: 'Maria', nascimento: '1942-09-12', ativo: true},
+	{ id: '4', nome: 'Pedro', nascimento: '1934-12-15', ativo: true},
+	{ id: '5', nome: 'Joana', nascimento: '1925-12-15', ativo: true},
+	{ id: '6', nome: 'Joca', nascimento: '1910-02-10', ativo: true},
+	{ id: '7', nome: 'Clementina', nascimento: '1940-07-24', ativo: false},
+	{ id: '8', nome: 'Manuela', nascimento: '1931-05-25', ativo: true},
+	{ id: '9', nome: 'Mauricio', nascimento: '1920-02-04', ativo: true},
+	{ id: '10', nome: 'Kleber', nascimento: '1912-03-09', ativo: false},
+	{ id: '11', nome: 'Juarez', nascimento: '1921-01-07', ativo: true},
+	{ id: '12', nome: 'Michelângelo', nascimento: '1927-08-01', ativo: true }
+];
+
