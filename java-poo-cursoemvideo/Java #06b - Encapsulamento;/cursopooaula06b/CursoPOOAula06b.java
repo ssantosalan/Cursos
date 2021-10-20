5d48fe3069271123e804793c2fcf21852f64b3da
Alan Santos <ss.alan@outlook.com>
2021-10-20 17:22:30 -0300

    commit

---
+++ java-poo-cursoemvideo/Java #06b - Encapsulamento;/cursopooaula06b/CursoPOOAula06b.java
@@ -0,0 +1,16 @@
+
+package cursopooaula06b;
+
+public class CursoPOOAula06b {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.ligarMudo();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+
+}
