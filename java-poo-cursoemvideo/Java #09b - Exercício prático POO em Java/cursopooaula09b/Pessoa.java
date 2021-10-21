e14f4b33b7deec7878e1a10075b06f0b0534c1aa
Alan Santos <ss.alan@outlook.com>
2021-10-21 09:58:24 -0300

    commit

---
+++ java-poo-cursoemvideo/Java #09b - Exercício prático POO em Java/cursopooaula09b/Pessoa.java
@@ -0,0 +1,48 @@
+
+package cursopooaula09b;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+    
+    //Métodos 
+    public void fazerAniver() {
+        this.idade++;
+    }
+    
+    //Construtor
+    public Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+    // Métodos especiais 
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    
+}
---
+++ java-poo-cursoemvideo/Java #09b - Exercício prático POO em Java/cursopooaula09b/Publicacao.java
@@ -0,0 +1,10 @@
+
+package cursopooaula09b;
+
+public interface Publicacao {
+    public abstract void abrir();
+    public abstract void fechar();
+    public abstract void folhear();
+    public abstract void avancarPag();
+    public abstract void voltarPag();
+}
