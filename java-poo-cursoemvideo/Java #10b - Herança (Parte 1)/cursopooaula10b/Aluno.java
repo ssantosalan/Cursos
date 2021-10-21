53d506006ce4c98fc3faa93524c73572bdd93020
Alan Santos <ss.alan@outlook.com>
2021-10-21 10:48:51 -0300

    commit

---
+++ java-poo-cursoemvideo/Java #10b - Herança (Parte 1)/cursopooaula10b/Aluno.java
@@ -0,0 +1,31 @@
+
+package cursopooaula10b;
+
+public class Aluno extends Pessoa {
+    private int matr;
+    private String curso;
+    
+    public void cancelarMatr() {
+        System.out.println("CANCELADA!");
+    }
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+    
+}
+
---
+++ java-poo-cursoemvideo/Java #10b - Herança (Parte 1)/cursopooaula10b/CursoPOOAula10b.java
@@ -0,0 +1,33 @@
+
+package cursopooaula10b;
+
+public class CursoPOOAula10b {
+
+    public static void main(String[] args) {
+           //Programa principal
+           Pessoa p1 = new Pessoa();
+           Aluno p2 = new Aluno();
+           Professor p3 = new Professor();
+           Funcionario p4 = new Funcionario();
+           
+           p1.setNome("Pedro");
+           p2.setNome("Maria");
+           p3.setNome("Cláudio");
+           p4.setNome("Fabiana");
+           
+           p1.setSexo("M");
+           p4.setSexo("F");
+           p2.setIdade(18);
+           
+           p2.setCurso("Informática");
+           p3.setSalario(2500.75f);
+           p4.setSetor("Estoque");
+           
+           System.out.println(p1.toString());
+           System.out.println(p2.toString());
+           System.out.println(p3.toString());
+           System.out.println(p4.toString());
+           
+    }
+
+}
