6778d2a9830a7cff568921a16cf9e20717bbf3a3
Alan Santos <ss.alan@outlook.com>
2021-10-22 10:19:03 -0300

    commit

--- angular-curso/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ angular-curso/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomeSite: string;
 
+  cursos: string[] = ['Java', 'Angular', 'C#'];
+
+  constructor() { 
+    this.nomeSite = 'http://bluesnova.com.br';
+  }
+    
   ngOnInit(): void {
   }
 
