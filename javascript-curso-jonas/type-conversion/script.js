e0858b1c383e30738c88ed4814ef344dae26935f
Alan Santos <ss.alan@outlook.com>
2021-10-29 11:16:03 -0300

    commit

---
+++ javascript-curso-jonas/type-conversion/script.js
@@ -0,0 +1,13 @@
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23));
+console.log(23);
+
+// Type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' + '10' + 3);
