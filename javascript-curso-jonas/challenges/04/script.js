616562080c0800d1e29a17065184a09a4c270b8c
Alan Santos <ss.alan@outlook.com>
2021-10-30 09:41:11 -0300

    commit

--- javascript-curso-jonas/challenges/04/script.js
+++ javascript-curso-jonas/challenges/04/script.js
@@ -1,3 +1,4 @@
 const bill = 275;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;  
-console.log(tip)
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20; 
+const totalValue = bill + tip; 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`)
