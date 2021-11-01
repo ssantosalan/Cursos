b2b83303cffe84fb4d2d7ad75017dffc671db5a8
Alan Santos <ss.alan@outlook.com>
2021-10-31 21:07:20 -0300

    commit

--- javascript-curso-jonas/02-fundamentals/script.js
+++ javascript-curso-jonas/02-fundamentals/script.js
@@ -1,11 +1,34 @@
-"use strict";
+/*
+// "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("Passed");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("Passed");
 
 //const interface = "Audio";
 //const private = 534;
-//const if = 23;        
+//const if = 23;     
+*/
+
+function logger(){
+    console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcecssor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcecssor(6, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcecssor(3,5);
+console.log(appleOrangeJuice);
+
