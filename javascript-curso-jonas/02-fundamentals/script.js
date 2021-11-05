212996960664c6a1a013feee9ae398e3cf26460e
Alan Santos <ss.alan@outlook.com>
2021-11-05 18:57:24 -0300

    commit

--- javascript-curso-jonas/02-fundamentals/script.js
+++ javascript-curso-jonas/02-fundamentals/script.js
@@ -446,13 +446,13 @@ const jonas = [
 ];
 
 for (let i = jonas.length - 1; i >= 0; i--) {
-  console.log(i , jonas[i]);
+  console.log(i, jonas[i]);
 }
 
 for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`---------- Starting exercise ${exercise}`);
 
   for (let rep = 1; rep < 6; rep++) {
-    console.log(`Lifting weight repetition ${rep}`)
+    console.log(`Lifting weight repetition ${rep}`);
   }
 }
