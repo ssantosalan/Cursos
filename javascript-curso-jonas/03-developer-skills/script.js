45253342a46d07ae61516110fa60a186dcbf542e
Alan Santos <ss.alan@outlook.com>
2021-11-08 12:11:49 -0300

    commit

--- javascript-curso-jonas/03-developer-skills/script.js
+++ javascript-curso-jonas/03-developer-skills/script.js
@@ -1,4 +1,19 @@
 "use strict";
 
-const xzz = 23;
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// - What is temp amplitude?  Answer: difference between highest and lowest temp
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let curTemp of temps) {
+    if (typeof curTemp !== "number") continue;
 
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
