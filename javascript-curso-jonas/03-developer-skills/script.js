465a6fc4860609943dde5b029b201611c98f14b4
Alan Santos <ss.alan@outlook.com>
2021-11-08 21:07:43 -0300

    commit

--- javascript-curso-jonas/03-developer-skills/script.js
+++ javascript-curso-jonas/03-developer-skills/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/*
+
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // - What is temp amplitude?  Answer: difference between highest and lowest temp
 const calcTempAmplitude = function (temps) {
@@ -39,3 +41,46 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // value: Number(prompt("Degrees celsius:")),
+    value: 10,
+  };
+
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify the bug
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let curTemp of temps) {
+    if (typeof curTemp !== "number") continue;
+
+    
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
