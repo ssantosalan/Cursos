93b47aa2633085e22bc970514f2cfcd7b184d581
Alan Santos <ss.alan@outlook.com>
2021-11-11 17:02:37 -0300

    commit

--- javascript-curso-jonas/05-guess-number-project/script.js
+++ javascript-curso-jonas/05-guess-number-project/script.js
@@ -1,5 +1,6 @@
 "strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "Correct Number!";
 console.log(document.querySelector(".message").textContent);
@@ -9,3 +10,53 @@ document.querySelector(".score").textContent = 22;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 30) + 1;
+let score = 5;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "❌ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    score += 10;
+    document.querySelector(".score").textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "💥 You've lost the game!";
+        score = 0;
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low! ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "💥 You've lost the game!";
+        score = 0;
+        document.querySelector(".score").textContent = 0;
+      if (score > 1) {
+        document.querySelector(".message").textContent = "📈 Too high!";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent =
+          "💥 You've lost the game!";
+        score = 0;
+        document.querySelector(".score").textContent = 0;
+      }
+    }
+  }
+});
