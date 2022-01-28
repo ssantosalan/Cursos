d564c3deb4b0b70e95e97b509b430701f7d5ff89
Alan Santos <ss.alan@outlook.com>
2022-01-28 09:28:13 -0300

    commit

--- javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
+++ javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
@@ -1,16 +1,33 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `You re ${age}, born in ${birthYear}`;
-    console.log(output);
-  }
-  printAge();
+//   function printAge() {
+//     const output = `You re ${age}, born in ${birthYear}`;
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age; 
-}
+//   return age;
+// }
 
-const firstName = `Jonas`;
-calcAge(1991);
+// const firstName = `Jonas`;
+// calcAge(1991);
+
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// // functions
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// //Example
