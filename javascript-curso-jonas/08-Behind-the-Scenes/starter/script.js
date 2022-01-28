3752927abe68dd7931d3d15fd6a361c1b2ba6d00
Alan Santos <ss.alan@outlook.com>
2022-01-28 11:46:28 -0300

    commit

--- javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
+++ javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
@@ -47,35 +47,38 @@
 // console.log('Friend:', friend);
 // console.log('Me', me);
 
-let lastName = 'Williams';
-let oldLastName = lastName;
-lastName = 'Davis';
-console.log(lastName, oldLastName);
+// let lastName = 'Williams';
+// let oldLastName = lastName;
+// lastName = 'Davis';
+// console.log(lastName, oldLastName);
 
-const jessica = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
-  age: 27,
-};
-const marriedJessica = jessica;
-marriedJessica.lastName = 'Davis';
-console.log('Before marriage:', jessica);
-console.log('After marriage:', marriedJessica);
+// const jessica = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+// };
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage:', jessica);
+// console.log('After marriage:', marriedJessica);
 
-// Copying objects
+// // Copying objects
 
-const jessica2 = {
-  firstName: 'Jessica',
-  lastName: 'Williams',
-  age: 27,
-  family: ['Alice,', 'Bob']
-};
+// const jessica2 = {
+//   firstName: 'Jessica',
+//   lastName: 'Williams',
+//   age: 27,
+//   family: ['Alice,', 'Bob']
+// };
 
-const jessicaCopy = Object.assign({}, jessica2);
-jessicaCopy.lastName = 'Davis';
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = 'Davis';
 
-jessicaCopy.family.push('Mary');
-jessicaCopy.family.push('John');
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John');
 
-console.log('Before marriage:', jessica2);
-console.log('After marriage:', jessicaCopy);
+// console.log('Before marriage:', jessica2);
+// console.log('After marriage:', jessicaCopy);
+
+//
+
