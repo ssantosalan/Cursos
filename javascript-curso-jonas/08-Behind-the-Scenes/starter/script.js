495067f46abcc02beb0c215569a803f6721981cc
Alan Santos <ss.alan@outlook.com>
2022-01-27 21:27:20 -0300

    commit

--- javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
+++ javascript-curso-jonas/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You re ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age; 
+}
+
+const firstName = `Jonas`;
+calcAge(1991);
