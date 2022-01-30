867e275a9f78a84ed28656dd204e24d64b1a998e
Alan Santos <ss.alan@outlook.com>
2022-01-30 14:15:35 -0300

    commit

--- javascript-curso-jonas/09-Data-Structures-Operators/starter/script.js
+++ javascript-curso-jonas/09-Data-Structures-Operators/starter/script.js
@@ -42,7 +42,49 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+// 1) Destructuring
+// SPREAD, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+};
+console.log(add(2, 3));
+console.log(add(5, 3, 7, 2));
+console.log(add(8, 2, 5, 3, 2, 1, 4));
+
+const x = [23, 5, 7];
+
+console.log(add(...x));
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives');
+restaurant.orderPizza('mushrooms');
 
 // real-world example
 // const ingredients = [
