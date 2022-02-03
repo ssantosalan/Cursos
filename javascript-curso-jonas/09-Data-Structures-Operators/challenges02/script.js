55cc06045112db7d409ff2a64a3a54d0c010fb01
Alan Santos <ss.alan@outlook.com>
2022-02-02 22:54:03 -0300

    commit

---
+++ javascript-curso-jonas/09-Data-Structures-Operators/challenges02/script.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Coding challenge 01
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+//2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+const [gp, ...fieldPlayers] = players1;
+
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+// const team1 = game.odds.team1;
+// const draw = game.odds.x;
+// const team2 = game.odds.team2;
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+console.log(team1, draw, team2);
+
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+// for (let player in players) {
+//   numberOfGoals += 1;
+// }
+// for (let i = 0; i <= players.length; i++){
+//   numberOfGoals += 1;
+// }
+
+const printGoals = function (...players) {
+  let numberOfGoals = 0;
+
+  players.forEach(function (name) {
+    numberOfGoals += 1;
+  });
+
+  console.log(...players, numberOfGoals);
+};
+
+printGoals(...game.scored);
+
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+console.log('----------Coding challenge 02----------');
+// Coding challenge 02
+
+//1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+//2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+
+let average = 0;
+const odds = Object.values(game.odds);
+console.log(odds);
+
+for (const number of odds) {
+  average += number;
+}
+
+console.log(average / odds.length);
+
+/*
+//3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+  Odd of victory Bayern Munich: 1.33
+  Odd of draw: 3.25
+  Odd of victory Borrussia Dortmund: 6.5
+  Get the team names directly from the game object, don't hardcode them
+  (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
+*/
+
+const properties = Object.keys(game.odds)
+console.log(properties);
+// console.log(Object.key(game.odds));
+
+
+
+
+
+
