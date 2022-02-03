4ff73cf710d779adcbef8a34a56232efc4a4d6c8
Alan Santos <ss.alan@outlook.com>
2022-02-03 11:41:35 -0300

    commit

--- javascript-curso-jonas/09-Data-Structures-Operators/challenges02/script.js
+++ javascript-curso-jonas/09-Data-Structures-Operators/challenges02/script.js
@@ -117,6 +117,7 @@ for (const number of odds) {
 
 console.log(average / odds.length);
 
+console.log('----------Coding challenge 03----------');
 /*
 //3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
   Odd of victory Bayern Munich: 1.33
@@ -126,12 +127,21 @@ console.log(average / odds.length);
   (except for "draw"). Hint: Note how the odds and the game objects have the same property names 😉
 */
 
-const properties = Object.keys(game.odds)
-console.log(properties);
-// console.log(Object.key(game.odds));
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
 
+console.log('----------Coding challenge 04----------');
+/*
+4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+{
+  Gnarby: 1,
+  Hummels: 1,
+  Lewandowski: 2
+}
+GOOD LUCK 😀
+*/
 
-
-
-
-
+// let scorers = {}
+// Object.keys(scorers) = { ...game.scored };
