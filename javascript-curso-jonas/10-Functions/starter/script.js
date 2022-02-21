740c3203abb3745a7fe23d7c97990f1881aa305b
Alan Santos <ss.alan@outlook.com>
2022-02-21 10:19:05 -0300

    commit

--- javascript-curso-jonas/10-Functions/starter/script.js
+++ javascript-curso-jonas/10-Functions/starter/script.js
@@ -1,31 +1,31 @@
 'use strict';
 
-const oneWord = function (str) {
-  return str.replaceAll(' ', '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replaceAll(' ', '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-  console.log(`Transformed by: ${fn.name}`);
-};
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer(`JavaScript is the best`, upperFirstWord);
-transformer(`JavaScript is the best`, oneWord);
+// transformer(`JavaScript is the best`, upperFirstWord);
+// transformer(`JavaScript is the best`, oneWord);
 
-//JS uses callbacks all the time
-const high5 = function () {
-  console.log('👋');
-}
+// //JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('👋');
+// }
 
-document.body.addEventListener('click', high5);
-['Jonas', 'Marth', 'Alan'].forEach(high5)
+// document.body.addEventListener('click', high5);
+// ['Jonas', 'Marth', 'Alan'].forEach(high5)
 
 // const bookings = [];
 
@@ -76,3 +76,65 @@ document.body.addEventListener('click', high5);
 
 // newPassport(jonas);
 // checkIn(flight, jonas);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting}, ${name}!`);
+//   };
+// };
+
+// const greeterHey = greet(`Hey`);
+
+// greeterHey(`Alan`);
+
+// greet('Hello')('Alan');
+
+//Arrow function
+// const greet = greeting => name => console.log(`${greeting}, ${name}!`);
+
+// const greeterHi = greet(`Hi`);
+// greeterHi(`Orlando`)
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   booking: [],
+
+//   book(flighNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flighNum}`
+//     );
+//     this.booking.push({
+//       flight: `${this.iataCode}${flighNum}`,
+//       name,
+//     });
+//   },
+// };
+
+// lufthansa.book(239, 'Alan Santos');
+// console.log(lufthansa.booking);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   booking: [],
+// };
+
+// const book = lufthansa.book;
+// // Does NOT wok
+// // book(23, 'Sarah');
+
+// book.call(eurowings, 23, 'Sarah');
+
+// const swiss = {
+//   airline: 'Swiss',
+//   iataCode: 'LH',
+//   booking: [],
+// }
+
+// // Apply method isn't used on modern javascript anymore
+// const flightData = [555, 'George C'];
+// book.apply = (swiss, flightData);
+// console.log(swiss);
+// // How we do now: 
+// book.call(swiss, ...flightData);
