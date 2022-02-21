3d27c840ccbca8c35347f13753c9655aa30a2088
Alan Santos <ss.alan@outlook.com>
2022-02-20 22:00:51 -0300

    commit

--- javascript-curso-jonas/10-Functions/starter/script.js
+++ javascript-curso-jonas/10-Functions/starter/script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassangers = 1,
+//   price = 199 * numPassangers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassangers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// //leave at default a parameter
+// createBooking('LH123', undefined, 2);
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Alan Santos',
+//   passport: 2346532,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH333';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 2346532) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random()) * 100000;
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
