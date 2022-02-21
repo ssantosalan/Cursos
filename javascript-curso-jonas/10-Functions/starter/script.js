1f639a6266be6ee8800cc29e6321115a54157272
Alan Santos <ss.alan@outlook.com>
2022-02-20 22:29:55 -0300

    commit

--- javascript-curso-jonas/10-Functions/starter/script.js
+++ javascript-curso-jonas/10-Functions/starter/script.js
@@ -1,6 +1,31 @@
 'use strict';
 
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
 
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer(`JavaScript is the best`, upperFirstWord);
+transformer(`JavaScript is the best`, oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+}
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Marth', 'Alan'].forEach(high5)
 
 // const bookings = [];
 
