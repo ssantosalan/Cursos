bcea4a00322196ada50e7732bd0e94fbfc574b9a
Alan Santos <ss.alan@outlook.com>
2022-02-22 10:38:05 -0300

    commit

--- javascript-curso-jonas/10-Functions/starter/script.js
+++ javascript-curso-jonas/10-Functions/starter/script.js
@@ -95,46 +95,86 @@
 // const greeterHi = greet(`Hi`);
 // greeterHi(`Orlando`)
 
-// const lufthansa = {
-//   airline: 'Lufthansa',
-//   iataCode: 'LH',
-//   booking: [],
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  booking: [],
 
-//   book(flighNum, name) {
-//     console.log(
-//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flighNum}`
-//     );
-//     this.booking.push({
-//       flight: `${this.iataCode}${flighNum}`,
-//       name,
-//     });
-//   },
-// };
+  book(flighNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flighNum}`
+    );
+    this.booking.push({
+      flight: `${this.iataCode}${flighNum}`,
+      name,
+    });
+  },
+};
 
-// lufthansa.book(239, 'Alan Santos');
-// console.log(lufthansa.booking);
+lufthansa.book(239, 'Alan Santos');
+console.log(lufthansa.booking);
 
-// const eurowings = {
-//   airline: 'Eurowings',
-//   iataCode: 'EW',
-//   booking: [],
-// };
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  booking: [],
+};
 
-// const book = lufthansa.book;
-// // Does NOT wok
-// // book(23, 'Sarah');
+const book = lufthansa.book;
+// Does NOT wok
+// book(23, 'Sarah');
 
-// book.call(eurowings, 23, 'Sarah');
+book.call(eurowings, 23, 'Sarah');
 
-// const swiss = {
-//   airline: 'Swiss',
-//   iataCode: 'LH',
-//   booking: [],
-// }
+const swiss = {
+  airline: 'Swiss',
+  iataCode: 'LH',
+  booking: [],
+};
 
-// // Apply method isn't used on modern javascript anymore
-// const flightData = [555, 'George C'];
-// book.apply = (swiss, flightData);
-// console.log(swiss);
-// // How we do now: 
-// book.call(swiss, ...flightData);
+// Apply method isn't used on modern javascript anymore
+const flightData = [555, 'George C'];
+book.apply = (swiss, flightData);
+console.log(swiss);
+// How we do now:
+book.call(swiss, ...flightData);
+
+// Bind Method
+const bookEW = book.bind(eurowings);
+bookEW(23, 'Steven W');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Alanx');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+console.log(lufthansa);
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+
+// Rewrite: one function return another function.
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
