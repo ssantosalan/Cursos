28cb9e489106eed50cb5878106bf5c5c01d6f99d
Alan Santos <ss.alan@outlook.com>
2022-02-23 18:05:38 -0300

    commit

--- javascript-curso-jonas/10-Functions/starter/script.js
+++ javascript-curso-jonas/10-Functions/starter/script.js
@@ -95,86 +95,169 @@
 // const greeterHi = greet(`Hi`);
 // greeterHi(`Orlando`)
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  booking: [],
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   booking: [],
 
-  book(flighNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flighNum}`
+//   book(flighNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flighNum}`
+//     );
+//     this.booking.push({
+//       flight: `${this.iataCode}${flighNum}`,
+//       name,
+//     });
+//   },
+// };
+
+// lufthansa.book(239, 'Alan Santos');
+// console.log(lufthansa.booking);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   booking: [],
+// };
+
+// const book = lufthansa.book;
+// // Does NOT wok
+// // book(23, 'Sarah');
+
+// book.call(eurowings, 23, 'Sarah');
+
+// const swiss = {
+//   airline: 'Swiss',
+//   iataCode: 'LH',
+//   booking: [],
+// };
+
+// // Apply method isn't used on modern javascript anymore
+// const flightData = [555, 'George C'];
+// book.apply = (swiss, flightData);
+// console.log(swiss);
+// // How we do now:
+// book.call(swiss, ...flightData);
+
+// // Bind Method
+// const bookEW = book.bind(eurowings);
+// bookEW(23, 'Steven W');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Alanx');
+
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// // lufthansa.buyPlane();
+// console.log(lufthansa);
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value * 0.23;
+
+// console.log(addVAT(100));
+
+// // Rewrite: one function return another function.
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+
+// Coding Challenge #1
+
+/* Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
+
+Your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+method does 2 things:
+
+1.1. Display a prompt window for the user to input the number of the
+selected option. The prompt should look like this:
+What is your favourite programming language?
+
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+
+1.2. Based on the input number, update the 'answers' array property. For
+example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes
+sense (e.g. answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+The Complete JavaScript Course 21
+Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+Hints: Use many of the tools you learned about in this and the last section 😉
+GOOD LUCK 😀 */
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      window.prompt(
+        `${this.question}
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)`
+      )
     );
-    this.booking.push({
-      flight: `${this.iataCode}${flighNum}`,
-      name,
-    });
+    if (answer >= 0 && answer < 4) {
+      this.answers[answer]++;
+    } else {
+      window.alert(`Não há essa alternativa.`);
+    }
+
+    displayResults(poll.answers);
   },
 };
 
-lufthansa.book(239, 'Alan Santos');
-console.log(lufthansa.booking);
-
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  booking: [],
-};
-
-const book = lufthansa.book;
-// Does NOT wok
-// book(23, 'Sarah');
-
-book.call(eurowings, 23, 'Sarah');
-
-const swiss = {
-  airline: 'Swiss',
-  iataCode: 'LH',
-  booking: [],
-};
-
-// Apply method isn't used on modern javascript anymore
-const flightData = [555, 'George C'];
-book.apply = (swiss, flightData);
-console.log(swiss);
-// How we do now:
-book.call(swiss, ...flightData);
-
-// Bind Method
-const bookEW = book.bind(eurowings);
-bookEW(23, 'Steven W');
-
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Alanx');
-
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-
-  this.planes++;
-  console.log(this.planes);
-};
-// lufthansa.buyPlane();
-console.log(lufthansa);
 document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-// Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-// addVAT = value => value + value * 0.23;
-
-console.log(addVAT(100));
-
-// Rewrite: one function return another function.
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
+// 3. Create a method 'displayResults' which displays the poll results.
+const displayResults = function (type) {
+  console.log();
+  if (Array.isArray(type)) {
+    console.log(poll.answers);
+  } else if (String.isString(type)) {
+    console.log(`Poll results are ${poll.answers}`);
+  }
 };
-
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
