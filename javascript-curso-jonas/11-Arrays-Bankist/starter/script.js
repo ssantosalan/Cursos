d4c399de9078b04d185b7459d479dab6eaeb1409
Alan Santos <ss.alan@outlook.com>
2022-03-13 10:24:50 -0300

    commit

--- javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
+++ javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
@@ -65,17 +65,12 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 
 /////////////////////////////////////////////////
 
-// SlICE
+/* // SlICE
 let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(arr.slice(-2));
 console.log(arr.slice(2, 4));
@@ -101,3 +96,42 @@ console.log(arr2);
 const letters = arr.concat(arr2);
 console.log(letters);
 console.log([...arr, ...arr2]);
+
+// Join */
+
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+movements.forEach(function(movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: withdraw ${Math.abs(movement)}`);
+  }
+}); */
+
+// // Map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`)
+// });
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`)
+// })
+
+
+
+
