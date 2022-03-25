5c9c6798e6d6f725eae7c3ea8dc219c8189721c5
Alan Santos <ss.alan@outlook.com>
2022-03-25 17:17:11 -0300

    commit

--- javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
+++ javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
@@ -483,3 +483,30 @@ console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit)); */
 
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+
+const overalBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// With chaining
+const overalBalance2 = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
+
+// flatMap
+const overalBalance3 = accounts
+  .flatMap(acc => acc.movements) // Only one level deep
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance3);
