9fde70ff38dd196e6d2c8351f7e28ab0bf4ad735
Alan Santos <ss.alan@outlook.com>
2022-03-26 20:56:45 -0300

    commit

--- javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
+++ javascript-curso-jonas/11-Arrays-Bankist/starter/script.js
@@ -535,3 +535,35 @@ console.log(movements);
 // Descending
 movements.sort((a, b) => b - a);
 console.log(movements); */
+
+// More Ways of Creating and Filling Arrays;
+
+const arr = [1, 2, 3];
+console.log(new Array(1, 2, 3));
+
+// Empty arrays + fill method
+const x = new Array(3);
+console.log(x);
+
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+});
