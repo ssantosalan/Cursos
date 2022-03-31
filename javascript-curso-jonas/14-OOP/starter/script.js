8441c0143158119cc0c750765c2b19082a51f704
Alan Santos <ss.alan@outlook.com>
2022-03-31 12:55:27 -0300

    commit

--- javascript-curso-jonas/14-OOP/starter/script.js
+++ javascript-curso-jonas/14-OOP/starter/script.js
@@ -57,29 +57,31 @@
 
 // Coding Challenge #1
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.speed);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.speed);
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes', 95);
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
 
-car1.accelerate();
-car1.brake();
+// car1.accelerate();
+// car1.brake();
 
-car2.accelerate();
-car2.brake();
+// car2.accelerate();
+// car2.brake();
 
-console.log(car1);
-console.log(car2);
+// console.log(car1);
+// console.log(car2);
+
+
