4320c974853725cc35115782778d756eaee139ba
Alan Santos <ss.alan@outlook.com>
2022-04-01 09:07:30 -0300

    commit

--- javascript-curso-jonas/14-OOP/starter/script.js
+++ javascript-curso-jonas/14-OOP/starter/script.js
@@ -84,4 +84,42 @@
 // console.log(car1);
 // console.log(car2);
 
+// Getters and Setters;
 
+// Class declaration
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  great() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
