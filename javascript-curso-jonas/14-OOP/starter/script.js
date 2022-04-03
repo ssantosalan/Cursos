f2faab01758c8bbcb57238e13c1bb51da8da4eed
Alan Santos <ss.alan@outlook.com>
2022-04-03 14:15:50 -0300

    commit

--- javascript-curso-jonas/14-OOP/starter/script.js
+++ javascript-curso-jonas/14-OOP/starter/script.js
@@ -225,3 +225,27 @@ const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 };
+
+Car.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge = this.charge * 0.99;
+  console.log(
+    `${this.make} going at ${this.speed}km/h, with a charge of ${this.charge}%`
+  );
+};
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+const carE01 = new EV('Tesla', 120, 80);
+
+carE01.accelerate();
+carE01.chargeBattery(90);
+console.log(carE01);
