4da12db1165d8e669029bb0f9b9f175431f1948d
Alan Santos <ss.alan@outlook.com>
2022-04-03 23:36:04 -0300

    commit

--- javascript-curso-jonas/14-OOP/starter/script.js
+++ javascript-curso-jonas/14-OOP/starter/script.js
@@ -342,27 +342,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for  opening an account, ${owner}`);
   }
 
-  // Public interface
+  // Public interfac
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val){
-      return true;
+  _approveLoan(val) {
+    return true;
   }
 
   requestLoan(val) {
-    if(this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -372,9 +377,9 @@ class Account {
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
 acc1.deposit(250);
-acc1.withdraw(140)
-acc1.requestLoan(1000)
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
-
-
+console.log(acc1._pin);
