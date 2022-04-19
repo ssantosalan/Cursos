5a2748ab14c7ada770a44af7cddd7360cb08e9e9
Alan Santos <ss.alan@outlook.com>
2022-04-18 22:07:18 -0300

    commit

--- javascript-curso-jonas/16-Asynchronous/starter/script.js
+++ javascript-curso-jonas/16-Asynchronous/starter/script.js
@@ -316,10 +316,57 @@ const whereAmI = function () {
 
 btn.addEventListener('click', whereAmI);
 
-'';
-
-
+('');
 
 // Coding challenge ##2
 
-const createImage = fu
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let curretImg;
+createImage('img/img-1.jpg')
+  .then(img => {
+    curretImg = img;
+    console.log('Imagem 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    curretImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    curretImg = img;
+    console.log('Imagem 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    curretImg.style.display = 'none';
+  })
+  .catch(err => console.log(err));
+
+// ----------------------- end coding challenge
+
+// const whereAmI = function(country) {
+
+// }
