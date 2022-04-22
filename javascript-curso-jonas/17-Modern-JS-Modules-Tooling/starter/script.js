c2d2303c888094006d2bf8f8b4350ff89d49a44e
Alan Santos <ss.alan@outlook.com>
2022-04-22 11:33:36 -0300

    commit with package

--- javascript-curso-jonas/17-Modern-JS-Modules-Tooling/starter/script.js
+++ javascript-curso-jonas/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,16 +9,66 @@ console.log('Importing module');
 // const data = await res.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts');
-  const data = await res.json();
-  //   console.log(data);
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
-const lastPost = getLastPost();
-console.log(lastPost);
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts');
+//   const data = await res.json();
+//   //   console.log(data);
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
 // console.log(getLastPost().then(res => console.log(res)));
 
-const  lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const  lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+// -----------
+
+/* const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+      addToCart,
+      cart,
+      totalPrice,
+      totalQuantity,
+      shippingCost
+  }
+})();
+
+ShoppingCart2.addToCart('apple', 4)
+ShoppingCart2.addToCart('pizza', 2)
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); */
+
+// --------------
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantify: 5 },
+    { product: 'pizza', quantify: 2 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
--- javascript-curso-jonas/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ javascript-curso-jonas/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -6,3 +6,16 @@ console.log('Exporting module');
 console.log("Start fetching users");
 await fetch('https://jsonplaceholder.typicode.com/users/1/posts');
 console.log('Finish fetching users');
+
+export const cart = [];
+const shoppingCart = 10;
+
+export const addToCart = function(product, quantity ) {
+    cart.push({product, quantity})
+    console.log(`${quantity} ${product} added to cart`);
+}
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq}
