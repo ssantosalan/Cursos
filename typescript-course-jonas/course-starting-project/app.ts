1ec0f427a5566fa64090e11157dcd7da3fb9ac8a
Alan Santos <ss.alan@outlook.com>
2022-04-23 22:26:02 -0300

    commit

--- typescript-course-jonas/course-starting-project/app.ts
+++ typescript-course-jonas/course-starting-project/app.ts
@@ -1,9 +1,13 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "alan",
   age: 26,
+  hobbies: ["Sports", "Cooking"],
 };
+
+let favoriteActivities: string[];
 
 console.log(person.name);
