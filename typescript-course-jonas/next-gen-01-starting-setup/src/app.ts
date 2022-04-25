24ef277719a77f6e25aadffc0fef514ca8434a4d
Alan Santos <ss.alan@outlook.com>
2022-04-25 09:27:13 -0300

    Commit

--- typescript-course-jonas/next-gen-01-starting-setup/src/app.ts
+++ typescript-course-jonas/next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,16 @@
 // Code goes here!
+
+const printOuput: (a: number, b: number) => void = (output) =>
+  console.log(output);
+
+printOuput(10, 1);
+
+const add = (...numbers: number[]) => {
+  console.log(numbers);
+  let result = numbers.reduce((acc, curr) => acc + curr, 0);
+  return result;
+};
+
+const addedNumbers = add(5, 5, 5, 5, 5);
+
+console.log(addedNumbers);
