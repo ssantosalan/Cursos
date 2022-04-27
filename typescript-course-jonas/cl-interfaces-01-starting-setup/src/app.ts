937cd52d4605979942adadc811ab4ba36f4763d5
Alan Santos <ss.alan@outlook.com>
2022-04-27 10:16:09 -0300

    Commit

--- typescript-course-jonas/cl-interfaces-01-starting-setup/src/app.ts
+++ typescript-course-jonas/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,12 +1,13 @@
-class Departament {
-  name: string;
+class Department {
+  // private id: string;
+  // public name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = name;
   }
-  describe(this: Departament) {
-    console.log(`Accounting: ${this.name}; ${this.employees}`);
+  describe(this: Department) {
+    console.log(`Department: ${this.name}; id: ${this.id}`);
   }
 
   addEmployees(employee: string) {
@@ -17,12 +18,45 @@ class Departament {
   }
 }
 
-const accounting = new Departament("accounting");
-accounting.describe();
+class itDepartment extends Department {
+  public admin: string[];
+  constructor(id: string, admin: string[]) {
+    super(id, "it department");
+    this.admin = admin;
+  }
+}
 
-// const accountingCopy = { name: "Alanzoka", describe: accounting.describe, employees: [] };
+class AccountingDepartment extends Department {
 
-// accountingCopy.describe();
+  constructor(id: string, public reports: string[]) {
+    super(id, "report department");
+    this.reports = reports;
+  }
+  addReport(text: string){
+    this.reports.push(text);
+  }
+  printReport(){
+    console.log(this.reports);
+    
+  }
+}
 
-accounting.addEmployees('Alan');
-accounting.printEmployees();
+const report = new AccountingDepartment("b2", ["vamo que vamo"]);
+report.addReport("Reportando");
+report.printReport();
+report.addEmployees("Bruna")
+report.printEmployees();
+console.log(report);
+
+
+const it = new itDepartment("10", ["Alann"]);
+
+it.describe();
+
+// const itCopy = { name: "Alanzoka", describe: it.describe, employees: [] };
+
+// itCopy.describe();
+
+it.addEmployees("Alan");
+it.printEmployees();
+console.log(it);
