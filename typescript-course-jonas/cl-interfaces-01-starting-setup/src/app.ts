0952b17d3f54c69e7e5589e433346e9d041bf649
Alan Santos <ss.alan@outlook.com>
2022-04-26 22:35:50 -0300

    Commit

--- typescript-course-jonas/cl-interfaces-01-starting-setup/src/app.ts
+++ typescript-course-jonas/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+class Departament {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+  describe(this: Departament) {
+    console.log(`Accounting: ${this.name}`);
+  }
+}
+
+const accounting = new Departament("accounting");
+accounting.describe();
+
+const accountingCopy = { name: "Alanzoka", describe: accounting.describe };
+
+accountingCopy.describe();
