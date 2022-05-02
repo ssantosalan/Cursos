f2a9f4d9218f854802ceb788b63e45d969e4490a
Alan Santos <ss.alan@outlook.com>
2022-05-02 10:01:02 -0300

    commit

--- javascript-curso-jonas/18-forkify/starter/src/js/controller.js
+++ javascript-curso-jonas/18-forkify/starter/src/js/controller.js
@@ -1,7 +1,7 @@
 // import icons from '../img/icons.svg' // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
 import 'core-js/stable';
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime';
 
 const { async } = require('q');
 
@@ -33,11 +33,16 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc3e'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcae1'
     );
     const data = await res.json();
@@ -163,4 +168,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe))
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
