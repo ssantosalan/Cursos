73458173db47eb69207e9cfc36a07d678746d983
Alan Santos <ss.alan@outlook.com>
2022-10-25 11:02:37 -0300

    codewars exercises

---
+++ codewars-exercises/exercises/Codewars/src/exercises/Alarm.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Alarm {
+	public static boolean setAlarm(boolean employed, boolean vacation) {
+		return employed == true && vacation == false ? true : false;
+	}
+}
--- codewars-exercises/exercises/Codewars/src/exercises/Teste.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Teste.java
@@ -1,17 +1,17 @@
 package exercises;
 
+import java.util.ArrayList;
+
 public class Teste {
 
 	public static void main(String[] args) {
-//
-//		System.out.println(DuplicateEncoder.encode("din"));
-//		System.out.println(DuplicateEncoder.encode("recede"));
-//		System.out.println(DuplicateEncoder.encode("Success"));
-//		System.out.println(DuplicateEncoder.encode("(( @"));
 
-		// expected:<))))[(]))))> but was:<))))[)]))))>
-		System.out.println(DuplicateEncoder.encode2("alana"));
-
+		ArrayList<String> cars = new ArrayList<>();
+		
+		cars.add("car");
+		
+		System.out.println(cars);
+		
 	}
 
 }
