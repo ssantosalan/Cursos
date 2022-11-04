17e7ea35585763b073af92df576f57e310804bb8
Alan Santos <ss.alan@outlook.com>
2022-11-04 10:44:44 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Teste.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Teste.java
@@ -1,14 +1,10 @@
 package exercises;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
 public class Teste {
 
 	public static void main(String[] args) {
 
-		System.out.println(TriangleTester.isTriangle(1, 5, 10));
+		System.out.println(Kata.switchItUp(10));
 
 	}
 
