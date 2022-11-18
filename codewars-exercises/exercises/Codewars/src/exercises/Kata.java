b0de7faea244283a5b8bb1cd9f8d97ec3287c445
Alan Santos <ss.alan@outlook.com>
2022-11-18 10:49:30 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Kata.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Kata.java
@@ -294,4 +294,15 @@ public class Kata {
 		return base % factor == 0;
 	}
 
+	public static long[] powersOfTwo(int n) {
+
+		long[] array = new long[n + 1];
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (long) Math.pow(2, i);
+		}
+
+		return array;
+	}
+
 }
--- codewars-exercises/exercises/Codewars/src/exercises/Teste.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Teste.java
@@ -7,14 +7,10 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		String word = "word";
-
-		System.out.println(Kata.solution(word));
-
-		int[] array = { 3, 2, 1 };
-
-		Arrays.stream(array).map(i -> i * 2).toArray();
-
+		
+		
+		System.out.println(Arrays.toString(Kata.powersOfTwo(1)));
+		
 	}
 
 }
