6eeb9bbea7f147bfe1a4c84eef8d397a28ca96bf
Alan Santos <ss.alan@outlook.com>
2022-12-02 14:44:10 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Kata.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Kata.java
@@ -321,4 +321,8 @@ public class Kata {
 		return beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length() - 1) == dish.charAt(dish.length() - 1);
 	}
 
+	public static int square(int n) {
+		return n * n;
+	}
+
 }
