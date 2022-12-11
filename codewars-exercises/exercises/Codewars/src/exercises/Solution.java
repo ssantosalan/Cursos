0d996d2e3cda8b13601f4e11b5686d85a11a7e1e
Alan Santos <ss.alan@outlook.com>
2022-12-11 11:28:36 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Solution.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Solution.java
@@ -146,4 +146,11 @@ public class Solution {
 		return Stream.of(s.split("")).map(i -> i + i).collect(Collectors.joining(""));
 	}
 
+	public static String correct(String string) {
+		string = string.replaceAll("[5]", "S");
+		string = string.replaceAll("[0]", "O");
+		string = string.replaceAll("[1]", "I");
+		return string;
+	}
+
 }
