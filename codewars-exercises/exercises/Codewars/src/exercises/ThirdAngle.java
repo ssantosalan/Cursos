5317b3076ac80a9dbf3109bba00310321a1c26b4
Alan Santos <ss.alan@outlook.com>
2022-12-19 20:53:15 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/ThirdAngle.java
+++ codewars-exercises/exercises/Codewars/src/exercises/ThirdAngle.java
@@ -4,4 +4,7 @@ public class ThirdAngle {
 	public static int otherAngle(int angle1, int angle2) {
 		return 180 - (angle1 + angle2);
 	}
+	// public static int otherAngle(int angle1, int angle2) {
+	// 	return 180 - (angle1 + angle2);
+	// }
 }
