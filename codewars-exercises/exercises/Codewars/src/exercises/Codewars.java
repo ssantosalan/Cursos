8cc87bc0503f9f31688e30fcead807f1f205cfde
Alan Santos <ss.alan@outlook.com>
2022-12-22 14:03:38 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Codewars.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Codewars.java
@@ -4,4 +4,7 @@ public class Codewars {
 	public static int strCount(String str, char letter) {
 		return str.replaceAll("[^"+letter+"]", "").length();
 	}
+	// public static int strCount(String str, char letter) {
+	// 	return str.replaceAll("[^"+letter+"]", "").length();
+	// }
 }
