09178606486e3bd60cbe6381b104ed67008ae68b
Alan Santos <ss.alan@outlook.com>
2022-12-26 14:15:52 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Solution.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Solution.java
@@ -220,5 +220,18 @@ public class Solution {
 	// public static boolean check(Object[] a, Object x) {
 	// 	return Arrays.asList(a).contains(x);
 	// }
-
+	// public static String whoLikesIt2(String... names) {
+	// 	switch (names.length) {
+	// 	case 0:
+	// 		return "no one likes this";
+	// 	case 1:
+	// 		return String.format("%s likes this", names[0]);
+	// 	case 2:
+	// 		return String.format("%s and %s like this", names[0], names[1]);
+	// 	case 3:
+	// 		return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+	// 	default:
+	// 		return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+	// 	}
+	// }
 }
