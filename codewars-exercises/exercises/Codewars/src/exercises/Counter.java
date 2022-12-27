3652126cc62d8ddd777c90b206d42e0251c4d220
Alan Santos <ss.alan@outlook.com>
2022-12-27 11:07:02 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Counter.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Counter.java
@@ -11,4 +11,14 @@ public class Counter {
 		}
 		return numberOfSheeps;
 	}
+	// public int countSheeps(Boolean[] arrayOfSheeps) {
+	// 	// TODO May the force be with you
+	// 	int numberOfSheeps = 0;
+	// 	for (Boolean sheep : arrayOfSheeps) {
+	// 		if (sheep != null && sheep) {
+	// 			numberOfSheeps++;
+	// 		}
+	// 	}
+	// 	return numberOfSheeps;
+	// }
 }
