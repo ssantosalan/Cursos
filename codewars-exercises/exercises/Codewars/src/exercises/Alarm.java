29275219fb4c15454da5fcad16c49278d517dc25
Alan Santos <ss.alan@outlook.com>
2023-01-02 11:04:14 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Alarm.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Alarm.java
@@ -4,4 +4,7 @@ public class Alarm {
 	public static boolean setAlarm(boolean employed, boolean vacation) {
 		return employed == true && vacation == false ? true : false;
 	}
+	// public static boolean setAlarm(boolean employed, boolean vacation) {
+	// 	return employed == true && vacation == false ? true : false;
+	// }
 }
