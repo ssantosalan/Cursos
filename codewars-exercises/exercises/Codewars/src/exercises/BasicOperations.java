4441b59126f610730f5d438a3872067186defdda
Alan Santos <ss.alan@outlook.com>
2023-01-05 18:58:56 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/BasicOperations.java
+++ codewars-exercises/exercises/Codewars/src/exercises/BasicOperations.java
@@ -37,3 +37,22 @@ public class BasicOperations {
 // 		}
 // 	}
 // }
+
+// public class BasicOperations {
+// 	public static Integer basicMath(String op, int v1, int v2) {
+// 		switch (op) {
+
+// 		case "+":
+// 			return v1 + v2;
+// 		case "-":
+// 			return v1 - v2;
+// 		case "*":
+// 			return v1 * v2;
+// 		case "/":
+// 			return v1 / v2;
+// 		default:
+// 			throw new IllegalArgumentException();
+
+// 		}
+// 	}
+// }
