14167df09580f3fe18d91ca1560d29b4171537f2
Alan Santos <ss.alan@outlook.com>
2023-01-06 12:03:04 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/TwiceAsOld.java
+++ codewars-exercises/exercises/Codewars/src/exercises/TwiceAsOld.java
@@ -4,6 +4,10 @@ package exercises;
 //(or in how many years he will be twice as old). 
 //The answer is always greater or equal to 0,
 //no matter if it was in the past or it is in the future.
+// Сalculate how many years ago the father was twice as old as his son 
+// (or in how many years he will be twice as old). 
+// The answer is always greater or equal to 0,
+// no matter if it was in the past or it is in the future.
 
 public class TwiceAsOld {
 	public static int TwiceAsOld(int dadYears, int sonYears) {
