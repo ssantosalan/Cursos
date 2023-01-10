010675e90710eab729b85caca4edd38fdbe9fbed
Alan Santos <ss.alan@outlook.com>
2023-01-10 08:15:00 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/TrafficLights.java
+++ codewars-exercises/exercises/Codewars/src/exercises/TrafficLights.java
@@ -26,3 +26,16 @@ public class TrafficLights {
 	// 	}
 	// }
 }
+
+// public static String updateLight(String current) {
+// 	switch (current) {
+// 	case "red":
+// 		return "green";
+// 	case "yellow":
+// 		return "red";
+// 	case "green":
+// 		return "yellow";
+// 	default:
+// 		throw new IllegalArgumentException();
+// 	}
+// }
