70228c2d46b3e41f4969f2b0ca0bd7cdefbff40b
Alan Santos <ss.alan@outlook.com>
2023-01-31 08:59:07 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/FindDivisor.java
+++ codewars-exercises/exercises/Codewars/src/exercises/FindDivisor.java
@@ -5,6 +5,7 @@ import java.util.stream.Stream;
 public class FindDivisor {
 	public long numberOfDivisors(int n) {
 		int cont = 0;
+		// int cont = 1;
 
 		for (int i = 1; i <= n; i++) {
 			if (n % i == 0) {
