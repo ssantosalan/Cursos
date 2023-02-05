b36548be68c6a6eebbce94ea0cb61cb096c49069
Alan Santos <ss.alan@outlook.com>
2023-02-05 10:31:14 -0300

    codewars exercises

--- codewars-exercises/exercises/Codewars/src/exercises/Goals.java
+++ codewars-exercises/exercises/Codewars/src/exercises/Goals.java
@@ -3,5 +3,6 @@ package exercises;
 public class Goals {
 	public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
 		return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+		// return laLigaGoals;
 	}
 }
