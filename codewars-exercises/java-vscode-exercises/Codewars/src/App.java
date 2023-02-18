d462c1a1b3a8c36fcd5ca4c150eb9a272580547f
Alan Santos <ss.alan@outlook.com>
2023-02-18 10:50:17 -0300

    java exercises

--- codewars-exercises/java-vscode-exercises/Codewars/src/App.java
+++ codewars-exercises/java-vscode-exercises/Codewars/src/App.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        List<String> list = new ArrayList<String>();
+        list.add("alan");
+    
+        System.out.println(list.toString());
+        int[] array = {2, 4, 1};
+        List<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(2);
+        numeros.add(11);
+        numeros.add(1);
+        Collections.sort(numeros, Comparator.naturalOrder());
+        System.out.println(numeros);
     }
 }
