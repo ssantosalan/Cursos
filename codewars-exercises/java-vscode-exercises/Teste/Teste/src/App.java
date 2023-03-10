a0b49573c36776528b30d96d0376586754365f1d
Alan Santos <ss.alan@outlook.com>
2023-03-10 09:22:11 -0300

    desafio

--- codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
+++ codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
@@ -1,5 +1,6 @@
 import java.lang.System.Logger;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,5 +9,7 @@ public class App {
         System.out.println("vamo");
         String[] Letras = "vamos".split("");
         System.out.println(Arrays.toString(Letras));
+        var lista = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        System.out.println(lista);
     }
 }
