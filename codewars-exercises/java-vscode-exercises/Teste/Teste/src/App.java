1be0bf4258c9eba5b3eee05536e2702d121d2bdf
Alan Santos <ss.alan@outlook.com>
2023-03-13 14:41:09 -0300

    teste revisão

--- codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
+++ codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
@@ -1,6 +1,7 @@
 import java.lang.System.Logger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,5 +12,22 @@ public class App {
         System.out.println(Arrays.toString(Letras));
         var lista = List.of(1, 2, 3, 4, 5, 6, 7, 8);
         System.out.println(lista);
+
+        Integer inteiro = 10;
+
+        int num = inteiro.intValue();
+
+        String[] array = new String[] { "5", "4", "3", "2", "1" };
+
+        int a = Integer.valueOf(Arrays.stream(array).collect(Collectors.joining("")));
+        int b = Integer.valueOf(String.join("", array));
+        
+        int c = Integer.parseInt(String.join("", array));
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
     }
 }
