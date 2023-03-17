0ec2a828933dacce37a2ab97f9cf16c123c1a36a
Alan Santos <ss.alan@outlook.com>
2023-03-17 14:27:12 -0300

    commit

--- codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
+++ codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
@@ -1,33 +1,20 @@
 import java.lang.System.Logger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("vamo");
-        String[] Letras = "vamos".split("");
-        System.out.println(Arrays.toString(Letras));
-        var lista = List.of(1, 2, 3, 4, 5, 6, 7, 8);
-        System.out.println(lista);
-
-        Integer inteiro = 10;
-
-        int num = inteiro.intValue();
-
-        String[] array = new String[] { "5", "4", "3", "2", "1" };
-
-        int a = Integer.valueOf(Arrays.stream(array).collect(Collectors.joining("")));
-        int b = Integer.valueOf(String.join("", array));
+        List<String> carroLista = new ArrayList<>();
         
-        int c = Integer.parseInt(String.join("", array));
-        
+        String carro = ""; 
+        for (int i = 1; i <= 5; i++) {
+            carro = "carro " + i;
+            carroLista.add(carro);
+        }
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-
+        System.out.println(carroLista);
     }
+
 }
