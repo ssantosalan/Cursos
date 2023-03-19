a56683395c5b4f38ae619ace42da4f5ff64ec307
Alan Santos <ss.alan@outlook.com>
2023-03-19 15:59:43 -0300

    commit

--- codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
+++ codewars-exercises/java-vscode-exercises/Teste/Teste/src/App.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("thefutureisnow");
+        var lista = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+
+        System.out.println(lista);
 
     }
 
