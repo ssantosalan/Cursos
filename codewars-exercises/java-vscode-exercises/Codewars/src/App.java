4cca78603a6167443b46f64c8c91cae200442d9a
Alan Santos <ss.alan@outlook.com>
2023-03-20 08:11:30 -0300

    commit

--- codewars-exercises/java-vscode-exercises/Codewars/src/App.java
+++ codewars-exercises/java-vscode-exercises/Codewars/src/App.java
@@ -4,17 +4,6 @@ import java.util.List;
 import humanos.Padeiro;
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("alan ");
-    System.out.println("teste");
-    System.out.println("teste");
-    System.out.println("vamos");
-    var list = List.of(1, 2, 3, 4, 5);
-    System.out.println(list);
-    System.out.println(list);
-
-    List<String> lista = new ArrayList<String>();
-    lista.add("a");
-    System.out.println(lista);
-
+    System.out.println("Alan");
   }
 }
