0727cebcf7bda4993981b87a4f95f6c4e823c04a
Alan Santos Everymind <alan.santos@everymind.com.br>
2023-09-05 10:21:54 -0300

    commit teste

--- java-test-static/java/src/dentro/App.java
+++ java-test-static/java/src/dentro/App.java
@@ -4,6 +4,7 @@ import testefora.ClasseFora;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello,  World!");
+        System.out.println("TESTE");
 
         Teste teste = new Teste();
         teste.testar();
